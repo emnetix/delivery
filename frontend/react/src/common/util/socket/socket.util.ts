9b9ba6ed574aaa140f448bece2f7e585151c30f4
jh_bae <jh_bae@falinux.com>
2025-01-09 16:23:48 +0900

    fix: 소켓 연결 주소 product로 변경

--- frontend/react/src/common/util/socket/socket.util.ts
+++ frontend/react/src/common/util/socket/socket.util.ts
@@ -97,5 +97,5 @@ export const SOCKET_ENT = {
   ON_DELIVERY: 'delivery'
 };
 
-export const useSocket = createUseSocket('http://localhost:8000', { transports: ['websocket'], timeout: 7000, autoConnect: false, path: '/api/v1/ws/ent02delivery'});
-// export const useSocket = createUseSocket('wws://delivery.emnetix.net', { transports: ['websocket'], timeout: 7000, autoConnect: false, path: '/api/v1/ws/ent02delivery'});
+// export const useSocket = createUseSocket('http://192.168.220.11:8000', { transports: ['websocket'], timeout: 7000, autoConnect: false, path: '/api/v1/ws/ent02delivery'});
+export const useSocket = createUseSocket('wss://delivery.emnetix.net', { transports: ['websocket'], timeout: 7000, autoConnect: false, path: '/api/v1/ws/ent02delivery'});
